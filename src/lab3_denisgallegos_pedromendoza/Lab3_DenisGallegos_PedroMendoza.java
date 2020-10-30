36dfba7e8d2eaf53d4b11546930600a345cca11c
Denis Jose <denisj.gallegos@unitec.edu>
2020-10-30 13:45:03 -0600

    Primer commit

---
+++ src/lab3_denisgallegos_pedromendoza/Lab3_DenisGallegos_PedroMendoza.java
@@ -0,0 +1,15 @@
+/*Programa de laboratorio 3 de semana 3 de la clase de programacion 2.
+ */
+package lab3_denisgallegos_pedromendoza;
+
+import java.util.Scanner; 
+
+public class Lab3_DenisGallegos_PedroMendoza {
+
+    static Scanner lectura = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        
+    }//Fin del MAIN.
+    
+}//Fin de la clase.
